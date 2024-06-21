69d9a6a3100b3035369019c157045d023113d25d
rianpuc <redlopes@sga.pucminas.br>
2024-06-21 19:28:54 -0300

    menu mobile completo

---
+++ public/assets/scripts/app.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-itens");
+    const icon = document.querySelector(".menu-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
